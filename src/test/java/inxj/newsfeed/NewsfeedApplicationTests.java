45afb91a0a6617ff8130c65f0a30f3ffd9da7d3e
jypLord <jygold2n@naver.com>
2025-04-07 23:00:18 +0900

    fisrt commit

---
+++ src/test/java/inxj/newsfeed/NewsfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package inxj.newsfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsfeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
