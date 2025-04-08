6f597c0843aec66efa6d53a34683f541a7c47ff2
jypLord <jygold2n@naver.com>
2025-04-08 12:21:13 +0900

    feat/#5
    feat: Create User Entity. Post, Comment, Friend OneToMany 설정. User 회원탈퇴 시간 필드 직접 작성!

---
+++ src/main/java/inxj/newsfeed/user/User.java
@@ -0,0 +1,48 @@
+package inxj.newsfeed.user;
+
+
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@NoArgsConstructor
+public class User extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @Column(unique = true)
+    private String username;
+
+    private String name;
+
+    private LocalDateTime birthday;
+
+    private String gender;
+
+    @Column(unique = true)
+    private String phoneNumber;
+
+    private String profileImageUrl;
+
+    private LocalDateTime deletedAt;
+
+    @OneToMany(mappedBy = "post_id", cascade = CascadeType.ALL)
+    private List<Post> posts = new ArrayList<>();
+
+    @OneToMany(mappedBy = "comment_id", cascade = CascadeType.ALL)
+    private List<Comment> comments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "freind_request_id", cascade = CascadeType.ALL)
+    private List<Friends> fridends = new ArrayList<>();
+}
