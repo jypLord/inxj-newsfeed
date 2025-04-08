e7970851306da877a8aa2a926012939642591a63
kmchaejin <chaejin040@gmail.com>
2025-04-08 14:23:47 +0900

    fix: Status Enum Label 변경 Status Enum의 PENDING label값을 요청 대기로 변경 이슈 번호 #5

--- src/main/java/inxj/newsfeed/friend/entity/Status.java
+++ src/main/java/inxj/newsfeed/friend/entity/Status.java
@@ -3,7 +3,7 @@ package inxj.newsfeed.friend.entity;
 public enum Status {
     ACCEPT("요청 수락"),
     REJECT("요청 거절"),
-    PENDING("요청 보류");
+    PENDING("요청 대기");
 
     private final String label;
 
