8d7966c906209b1e37d17587b68b1e6ba25cb910
jypLord <jygold2n@naver.com>
2025-04-10 07:20:59 +0900

    style: 변경 줄바꿈

---
+++ src/main/java/inxj/newsfeed/user/entity/User.java
@@ -0,0 +1,48 @@
+package inxj.newsfeed.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+package inxj.newsfeed.user;
+
+import inxj.newsfeed.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+@Setter
+@Entity
+@Getter
+@NoArgsConstructor
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true)
+    private String username;
+
+    private String name;
+
+    private LocalDateTime birthday;
+
+    private String gender;
+
+    @Column(unique = true)
+    private String phoneNumber;
+
+    private String profileImageUrl;
+
+    private LocalDateTime deletedAt;
+}
