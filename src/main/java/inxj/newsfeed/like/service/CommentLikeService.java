ea749ce5d2a6720fee5eebe26edd4a30159d2507
jiyun-im-dev <jiyun.im.dev@gmail.com>
2025-04-09 20:33:01 +0900

    feat: 추가 중복 체크 로직
    이미 좋아요한 게시글/댓글인지 체크하는 로직 추가
    
    Fixes: #7

--- src/main/java/inxj/newsfeed/like/service/CommentLikeService.java
+++ src/main/java/inxj/newsfeed/like/service/CommentLikeService.java
@@ -1,10 +1,12 @@
 package inxj.newsfeed.like.service;
 
+import inxj.newsfeed.comment.Comment;
+import inxj.newsfeed.exception.CustomException;
+import inxj.newsfeed.exception.ErrorCode;
 import inxj.newsfeed.like.entity.CommentLike;
 import inxj.newsfeed.like.entity.CommentLikeId;
 import inxj.newsfeed.like.repository.CommentLikeRepository;
 import inxj.newsfeed.user.User;
-import inxj.newsfeed.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,20 +15,29 @@ import org.springframework.stereotype.Service;
 public class CommentLikeService {
 
     private final CommentLikeRepository commentLikeRepository;
-    private final CommentRepository commentRepository;
-    private final UserRepository userRepository;
     private final EntityFetcher entityFetcher;
 
     public void like(Long commentId, Long userId) {
+        // 복합키 생성
         Comment comment = entityFetcher.getCommentOrThrow(commentId);
         User user = entityFetcher.getUserOrThrow(userId);
         CommentLikeId commentLikeId = new CommentLikeId(commentId, userId);
+
+        // 중복 체크
+        if (commentLikeRepository.findById(commentLikeId).isPresent()) {
+            throw new CustomException(ErrorCode.CONFLICT_STATUS);
+        }
+
+        // 저장
         commentLikeRepository.save(new CommentLike(commentLikeId, comment, user));
     }
 
     public void unlike(Long commentId, Long userId) {
+        // 복합키로 데이터 검색
         CommentLikeId commentLikeId = new CommentLikeId(commentId, userId);
         CommentLike found = entityFetcher.getCommentLikeOrThrow(commentLikeId);
+
+        // 삭제
         commentLikeRepository.delete(found);
     }
 
