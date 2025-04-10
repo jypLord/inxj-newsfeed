89b3c3b8d8c835efcb79cbe7c6a0a33a306ef831
jiyun-im-dev <jiyun.im.dev@gmail.com>
2025-04-10 16:34:15 +0900

    chore: 삭제 주석
    완료된 TODO 주석 삭제

--- src/main/java/inxj/newsfeed/common/filter/LoginFilter.java
+++ src/main/java/inxj/newsfeed/common/filter/LoginFilter.java
@@ -40,7 +40,6 @@ public class LoginFilter extends OncePerRequestFilter {
 
         HttpSession session = request.getSession(false); // 세션이 없으면 null
 
-        // TODO: 세션에 어떤 이름으로 로그인 유저 속성을 담는지 확인 필요
         if (session == null || session.getAttribute("loginUser") == null) {
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401
             response.setContentType("application/json");
