427aa4f9efa66e4dfa369ba31237165a5dc2a9d3
jiyun-im-dev <jiyun.im.dev@gmail.com>
2025-04-10 19:16:54 +0900

    fix: 수정 유저 리포지토리 쿼리 오타
    테이블명 users -> user로 오타 수정

--- src/main/java/inxj/newsfeed/user/repository/UserRepository.java
+++ src/main/java/inxj/newsfeed/user/repository/UserRepository.java
@@ -14,6 +14,6 @@ public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByPhoneNumber(String phoneNumber);
 
     // 검색한 문자가 포함돼있는 유저의 닉네임과 프로필 사진 가져오기
-    @Query(value = "SELECT username, profileImageUrl FROM users WHERE username LIKE CONCAT('%', ?1, '%')", nativeQuery = true)
+    @Query(value = "SELECT username, profileImageUrl FROM user WHERE username LIKE CONCAT('%', ?1, '%')", nativeQuery = true)
     List<User> findByUsername(String username);
 }
