bc91fb0425a366d0b0526343d4ec635bd8dfe940
kmchaejin <chaejin040@gmail.com>
2025-04-12 17:14:58 +0900

    refactor: 친구 도메인 httpSession 대신 어노테이션 사용

---
+++ src/main/java/inxj/newsfeed/common/constant/Const.java
@@ -0,0 +1,5 @@
+package inxj.newsfeed.common.constant;
+
+public interface Const {
+    String LOGIN_USER = "loginUser";
+}
--- src/main/java/inxj/newsfeed/friend/controller/FriendController.java
+++ src/main/java/inxj/newsfeed/friend/controller/FriendController.java
@@ -22,8 +22,7 @@ public class FriendController {
     친구 목록 조회 API
      */
     @GetMapping("/friends")
-    public ResponseEntity<List<FriendResponseDto>> findAllFriends(HttpSession session){
-        Long userId = (Long) session.getAttribute("loginUser");
+    public ResponseEntity<List<FriendResponseDto>> findAllFriends(@SessionAttribute("loginUser") Long userId){
         return new ResponseEntity<>(friendService.findAllFriends(userId), HttpStatus.OK);
     }
 
@@ -31,8 +30,7 @@ public class FriendController {
     친구 삭제 API
      */
     @DeleteMapping("/friends/{targetId}")
-    public ResponseEntity<Void> deleteFriend(HttpSession session, @PathVariable Long targetId){
-        Long loginUserId = (Long) session.getAttribute("loginUser");
+    public ResponseEntity<Void> deleteFriend(@SessionAttribute("loginUser") Long loginUserId, @PathVariable Long targetId){
         friendService.deleteFriend(loginUserId, targetId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
@@ -41,8 +39,7 @@ public class FriendController {
     친구 요청 API
      */
     @PostMapping("/friend-requests/{targetId}")
-    public ResponseEntity<Void> requestFriend(HttpSession session, @PathVariable Long targetId){
-        Long loginUserId = (Long) session.getAttribute("loginUser");
+    public ResponseEntity<Void> requestFriend(@SessionAttribute("loginUser") Long loginUserId, @PathVariable Long targetId){
         friendService.requestFriend(loginUserId, targetId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
@@ -51,8 +48,7 @@ public class FriendController {
     친구 요청 수락 API
      */
     @PostMapping("/friend-requests/accept/{targetId}")
-    public ResponseEntity<Void> acceptRequest(HttpSession session, @PathVariable Long targetId){
-        Long loginUserId = (Long) session.getAttribute("loginUser");
+    public ResponseEntity<Void> acceptRequest(@SessionAttribute("loginUser") Long loginUserId, @PathVariable Long targetId){
         friendService.acceptRequest(loginUserId, targetId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
@@ -61,8 +57,7 @@ public class FriendController {
     친구 요청 거절 API
      */
     @PostMapping("/friend-requests/reject/{targetId}")
-    public ResponseEntity<Void> rejectRequest(HttpSession session, @PathVariable Long targetId){
-        Long loginUserId = (Long) session.getAttribute("loginUser");
+    public ResponseEntity<Void> rejectRequest(@SessionAttribute("loginUser") Long loginUserId, @PathVariable Long targetId){
         friendService.rejectRequest(loginUserId, targetId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
@@ -71,8 +66,7 @@ public class FriendController {
     보낸 친구 요청 목록 조회 API
      */
     @GetMapping("/friend-requests/sent")
-    public ResponseEntity<List<FriendRequestWithStatusResponseDto>> findSentRequests(HttpSession session){
-        Long userId = (Long) session.getAttribute("loginUser");
+    public ResponseEntity<List<FriendRequestWithStatusResponseDto>> findSentRequests(@SessionAttribute("loginUser") Long userId){
         return new ResponseEntity<>(friendService.findSentRequests(userId), HttpStatus.OK);
     }
 
@@ -80,8 +74,7 @@ public class FriendController {
     받은 친구 요청 목록 조회 API
      */
     @GetMapping("/friend-requests/received")
-    public ResponseEntity<List<FriendRequestResponseDto>> findReceivedRequests(HttpSession session){
-        Long userId = (Long) session.getAttribute("loginUser");
+    public ResponseEntity<List<FriendRequestResponseDto>> findReceivedRequests(@SessionAttribute("loginUser") Long userId){
         return new ResponseEntity<>(friendService.findReceivedRequests(userId), HttpStatus.OK);
     }
 }
